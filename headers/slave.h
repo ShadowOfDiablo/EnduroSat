f58637a11b37b6714fa9867764a1dcd6c3a16b96
Tihomir Dimitrov <114056948+ShadowOfDiablo@users.noreply.github.com>
2025-07-08 00:01:49 +0300

    Delete old headers directory

--- headers/slave.h
+++
@@ -1,7 +0,0 @@
-#include "stdio.h"
-enum dev2_state {SLEEP,ACTIVE,FAULT};
-struct device
-{
-    unsigned int id;
-    enum dev2_state currentState;
-};
--- headers/stubs/stack_overflow.c
+++
@@ -1,9 +0,0 @@
-#include "FreeRTOS.h"
-#include "task.h"
-#include <stdio.h>
-
-void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
-    (void) xTask;
-    printf("Stack overflow in task: %s\n", pcTaskName);
-    for(;;);
-}
