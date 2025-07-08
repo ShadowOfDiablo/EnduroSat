e8698dd9eb9b47a807ffff4c36686807155f94cb
Tihomir Dimitrov <114056948+ShadowOfDiablo@users.noreply.github.com>
2025-07-08 12:43:07 +0300

    Updated build script
    It will now recognize the OS and compile it for the right OS(currently tested on Windows and Git Bash)

--- simulation/build.sh
+++ simulation/build.sh
@@ -1,4 +1,27 @@
+#!/bin/bash
 rm -rf build
 mkdir build && cd build
-cmake -G "MinGW Makefiles" ..
-make
+
+if [[ "$OSTYPE" == "msys"* ]]; then
+    echo "Building for Windows..."
+    cmake -G "MinGW Makefiles" ..
+    make
+    executable="device1_sim.exe"
+    # I am using Git Bash(MSYS) 
+    if [[ -n "$MSYSTEM" ]]; then
+        echo "Running in Git Bash"
+        winpty ./$executable
+    else
+        ./$executable
+    fi
+    
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Building for Linux..."
+    cmake ..
+    make
+    ./device1_sim
+    
+else
+    echo "Unsupported OS: $OSTYPE"
+    exit 1
+fi
